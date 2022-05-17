281ed1e011d03963ca29c2f3b99739884033a64c
ANEL <anel.moldabayeva@gmail.com>
2022-05-17 23:18:10 +0600

    added reset password redux

--- src/component/Signin/index.js
+++ src/component/Signin/index.js
@@ -1,184 +1,123 @@
-// import React from 'react';
-// import { 
-//   StyledFormArea, 
-//   StyledFormButton, 
-//   StyledTitle, 
-//   colors, 
-//   StyledContainer, 
-//   ButtonGroup, 
-//   ExtraText, 
-//   TextLink
-// } from '../Styles';
-
-// //formik and yup
-// import { Formik, Form } from 'formik';
-// import { TextInput } from '../FormLib';
-// import * as Yup from 'yup';
-
-// //icons
-// import {
-//   FiMail, 
-//   FiLock
-// } from 'react-icons/fi';
-
-// //auth and redux
-// import {connect} from 'react-redux';
-// import {loginUser} from "./../../auth/actions/userActions";
-// import {useNavigate} from "react-router-dom";
-
-
-// const Login = ({loginUser}) => {
-//   const history = useNavigate();
-//   return (
-//     <>
-//     <div> 
-//       <StyledContainer>
-      
-//       {/* <Icon to="/"> Airport System</Icon> */}
-//       <StyledFormArea>
-//         <StyledTitle color={colors.theme} size={25}> Sign in to your account </StyledTitle>
-//         <Formik
-//         initialValues={{
-//           email: "",
-//           password: "",
-//         }}
-//         validationSchema={
-//           Yup.object({
-//             email: Yup.string().email("Invalid email address")
-//             .required("Required"),
-//             password: Yup.string()
-//             .min(8, "Password is too short")
-//             .max(15, "Password is too long")
-//             .required("Required"),
-//           })
-//         }
-
-//         onSubmit={(values, {setSubmitting, setFieldError}) => {
-//           console.log(values);
-//           loginUser(values, history, setFieldError, setSubmitting);
-//         }}
-        
-//         >
-//           {() => (
-//             <Form>
-//               <TextInput 
-//               name="email"
-//               type="text"
-//               label="Email Address"
-//               placeholder="joe@example.com"
-//               icon={<FiMail/>}
-//               />
-
-//              <TextInput 
-//               name="password"
-//               type="password"
-//               label="Password"
-//               placeholder="********"
-//               icon={<FiLock/>}
-//               />
-
-//               <ButtonGroup>
-//                 <StyledFormButton type="submit">
-//                   Sign In
-//                 </StyledFormButton>
-//               </ButtonGroup>
-
-              
-
-//             </Form>
-//           )}
-//         </Formik>
-//         <ExtraText>
-//           Forgetten password? <TextLink to="/forgottenpassword">Reset it</TextLink>
-//         </ExtraText>
-//         <ExtraText>
-//           New here? <TextLink to="/signup">Signup</TextLink>
-//         </ExtraText>
-//       </StyledFormArea>
-      
-//       </StyledContainer>
-//     </div>
-//     </>
-    
-//   );
-// };
-
-// export default connect(null, {loginUser})(Login);
 import React, { useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { login } from '../../auth/actions/auth';
-import axios from 'axios';
+import {link, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import { 
+  StyledFormArea, 
+  StyledFormButton, 
+  StyledTitle, 
+  colors, 
+  StyledContainer, 
+  ButtonGroup, 
+  ExtraText, 
+  TextLink,
+  StyledTextInput
+} from '../Styles';
+//import PhoneInput from 'react-phone-input-2'
+
+import {login} from './../../auth/actions/auth';
 
 
-// const Navigate = useNavigate()
+const Login = ({login,  isAuthenticated }) => {
+  const [formData, setFromData] = useState({
+    email: '',
+    phoneNumber: '',
+    password: ''
+  });
 
-const Login = ({ login, isAuthenticated }) => {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '' 
-    });
+  const {email, phoneNumber, password} = formData;
 
-    const { email, password } = formData;
+  //const [errors, setErrors] = useState({});
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChange = e => setFromData({
+    ...formData,
+    [e.target.name]: e.target.value
+  });
 
-    const onSubmit = e => {
-        e.preventDefault();
+  const onSubmit = e =>{
+    e.preventDefault();
+    login(email, phoneNumber, password)
+  };
 
-        login(email, password);
-    };
+  if (isAuthenticated) {
+    return <Redirect to='/' />
+}
+  return (
+    <div> 
+      <StyledContainer>
+      {/* <Icon to="/"> Airport System</Icon> */}
+      <StyledFormArea>
+        <StyledTitle color={colors.theme} size={25}> Sign in to your account </StyledTitle>
+        
+            <form onSubmit={ e => onSubmit(e)}>
+              <StyledTextInput 
+              name="email"
+              type='email'
+              label={"Email Address"}
+              placeholder="Email Address"
+              value={email}
+              onChange={ e => onChange(e)}
+              required
+              />
 
-   
-  
+              {/* <PhoneInput
+               placeholder="Enter phone number"
+               specialLabel={''}
+               country={'kz'}
+               inputStyle={{
+                 borderColor: (props.touched && props.error) && "red"
+               }}
+               {...props}
+               />
+               {(props.touched && props.error) && <p style={{color:'red'}} >{props.error}</p> } */}
+           
+              {/* name="phoneNumber"
+              type="phone"
+              label="Phone number"
+              placeholder="Phone Number"
+              value={phoneNumber}
+              onChange={ e => onChange(e)}
+              required
+              /> */}
+              
+              <StyledTextInput 
+              name="password"
+              type="password"
+              label='Password'
+              placeholder="Password"
+              value={password}
+              onChange={ e => onChange(e)}
+              required
+              />
 
-    if (isAuthenticated) {
-        return <Navigate to='/' />
-    }
-
-    return (
-        <div className='container mt-5'>
-            <h1>Sign In</h1>
-            <p>Sign into your Account</p>
-            <form onSubmit={e => onSubmit(e)}>
-                <div className='form-group'>
-                    <input
-                        className='form-control'
-                        type='email'
-                        placeholder='Email'
-                        name='email'
-                        value={email}
-                        onChange={e => onChange(e)}
-                        required
-                    />
-                </div>
-                <div className='form-group'>
-                    <input
-                        className='form-control'
-                        type='password'
-                        placeholder='Password'
-                        name='password'
-                        value={password}
-                        onChange={e => onChange(e)}
-                        minLength='6'
-                        required
-                    />
-                </div>
-                <button className='btn btn-primary' type='submit'>Login</button>
+              <ButtonGroup>
+                <StyledFormButton type="submit">
+                  Sign In
+                </StyledFormButton>
+              </ButtonGroup>
             </form>
-            
-            <p className='mt-3'>
-                Don't have an account? <Link to='/signup'>Sign Up</Link>
-            </p>
-            <p className='mt-3'>
-                Forgot your Password? <Link to='/reset-password'>Reset Password</Link>
-            </p>
-        </div>
-    );
-};
+          
+        <ExtraText>
+          Forgot your password? <TextLink to="/reset-password">Reset it</TextLink>
+        </ExtraText>
+        <ExtraText>
+          New here? <TextLink to="/signup">Signup</TextLink>
+        </ExtraText>
 
+
+      </StyledFormArea>
+      
+      </StyledContainer>
+    </div>
+
+  );
+
+};
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated
+  isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps, { login })(Login);
+// export default connect(null,{login} ) (Login);
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,20 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-//auth stuff
-import {Provider} from 'react-redux';
-import store from './auth/store';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store = {store} >
     <App />
-
-
-    </Provider>
     
   </React.StrictMode>,
   document.getElementById('root')
